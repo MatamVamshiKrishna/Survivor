2c44ce8872541e693d976a432aeb619d19336851
Vamshi Krishna <matamvamshikrishna@gmail.com>
2020-01-14 07:15:45 +0530

    Fix movement and isGrounded (Temporary solution)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,95 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float rotationSpeed = 5.0f;
+    public float walkSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+    public float rotationSpeed = 30.0f;
+    public float jumpSpeed = 20.0f;
 
     private Vector3 moveDirection = Vector3.zero;
-    private bool isSprinting = false;
-    private bool canJump = false;
-    private float jumpSpeed = 10;
+    private bool canSprint = false;
+    private float speed;
 
     // component references
     private CharacterController characterController;
     private Animator animator;
-    //private bool canMove = false;
-    //private Vector3 movePos = Vector3.zero;
 
-
-    
-
-    // Start is called before the first frame update
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(characterController.isGrounded);
-        //if (characterController.isGrounded)
+        speed = walkSpeed;
+        canSprint = false;
+
+        if(Input.GetKey(KeyCode.LeftShift))
         {
-            moveDirection = new Vector3(0,0, Input.GetAxis("Vertical"));
-            transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0));
+            speed = sprintSpeed;
+            canSprint = true;
+        }
+
+
+        if(characterController.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
-        }
-        
+            moveDirection *= speed;
 
-        moveDirection.y -= 9.8f;
-
-        characterController.Move(moveDirection);
-
-
-
-        /*if(Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            isSprinting = true;
-        }
-
-        if(Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            isSprinting = false;
-        }*/
-
-        /*if(Input.GetKeyDown(KeyCode.Space))
-        {
-            //if (characterController.isGrounded)
+            if(Input.GetKeyDown(KeyCode.Space))
             {
-                Debug.Log("Grounded");
-                //canJump = true;
-                animator.SetBool("canJump", true);
-            }
-                
-        }*/
+                moveDirection.y = jumpSpeed;
+             }
+        }
 
-        
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0));
+        moveDirection.y -= 9.8f * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
 
-        // move only x and z axis
-       // moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-         // rotate around y axis
-        
-
-        // add gravity
-        //moveDirection.y -= 9.8f * Time.deltaTime;
-        //characterController.Move(moveDirection * speed * Time.deltaTime);
-        
-        animator.SetFloat("speed", characterController.velocity.magnitude);
-        //animator.SetBool("isSprinting", isSprinting);
-        
-        
-
-
-
-
-        
-
-        
-            
-
-       
+        var magnitude = new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
+        animator.SetFloat("speed", magnitude);
+        animator.SetBool("canSprint", canSprint);
         
     }
 }
